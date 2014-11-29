b766937e414ba8ee1d4a07d7a3bb5c5be26db730
bparrish206 <brentparrish@Rosias-MacBook-Pro.local>
2014-11-29 15:18:38 -0800

    finsished routes

--- app/js/client.js
+++ app/js/client.js
@@ -4,4 +4,5 @@ var $ = require('jquery');
 
 $.post('/', function(data){
     $('#app').append('<h4>' + data.chart + " " + data.date + " " + data.choice + " " + data.value + '%'+ '</h4>');
+    $('#main').append('<p>' + data.block+ '</p>');
   });
--- server.js
+++ server.js
@@ -1,26 +1,10 @@
 'use strict';
 
 var express = require('express');
-var request = require('superagent');
 var app = express();
 
 app.use(express.static(__dirname + '/build'));
 
-app.post('/', function(req, res){
-  var purl = 'http://elections.huffingtonpost.com/pollster/api/charts/obama-job-approval';
-  request
-    .get(purl)
-    .end(function (err, purlData){
-      if(err) throw err;
-      var title = purlData.body.title;
-      var est = purlData.body.estimates;
-      var choice = est[1].choice;
-      var value = est[1].value;
-      var date = purlData.body.last_updated;
-      var rdate = date.slice(0,10);
-    res.json({chart: title, date: rdate, choice: choice, value: value});
-  });
-});
-
+require('./routes/app_routes')(app);
 
 app.listen(3000);

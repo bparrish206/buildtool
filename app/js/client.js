1d281f9d8b9a118c117c24cdc91ed28820f0f7a8
bparrish206 <brentparrish@Rosias-MacBook-Pro.local>
2015-01-06 16:53:15 -0800

    tweak

--- app/js/client.js
+++ app/js/client.js
@@ -1,28 +1,6 @@
 'use strict';
 
 var $ = require('jquery');
-var request = require('superagent');
-
-$.post('/', function(req, res){
-  var purl = 'http://elections.huffingtonpost.com/pollster/api/charts/obama-job-approval';
-  request
-  .get(purl)
-  .end(function (err, purlData){
-    if(err) throw err;
-    var title = purlData.body.title;
-    var est = purlData.body.estimates;
-    var choice = est[1].choice;
-    var value = est[1].value;
-    var date = purlData.body.last_updated;
-    var block = JSON.stringify(purlData.body);
-    block = block.replace(/[{}]/g,'');
-    block = block.replace(/[""]/g,'');
-    block = block.replace(/[:]/g, ': ');
-    block = block.replace(/[,]/g, ', ');
-    var rdate = date.slice(0,10);
-    res.json({chart: title, date: rdate, choice: choice, value: value, block: block});
-  });
-});
 
 $.post('/', function(data) {
   $('#app').append('<h4>' + data.chart + ' ' + data.date + ' ' + data.choice + ' ' + data.value + '%' + '</h4>');
--- server.js
+++ server.js
@@ -2,9 +2,30 @@
 
 var express = require('express');
 var app = express();
+var request = require('superagent');
 
 app.use(express.static(__dirname + '/build'));
+app.post('/', function(req, res){
+  var purl = 'http://elections.huffingtonpost.com/pollster/api/charts/obama-job-approval';
+  request
+  .get(purl)
+  .end(function (err, purlData){
+    if(err) throw err;
+    var title = purlData.body.title;
+    var est = purlData.body.estimates;
+    var choice = est[1].choice;
+    var value = est[1].value;
+    var date = purlData.body.last_updated;
+    var block = JSON.stringify(purlData.body);
+    block = block.replace(/[{}]/g,'');
+    block = block.replace(/[""]/g,'');
+    block = block.replace(/[:]/g, ': ');
+    block = block.replace(/[,]/g, ', ');
+    var rdate = date.slice(0,10);
+    res.json({chart: title, date: rdate, choice: choice, value: value, block: block});
+  });
+});
 
-require('./routes/app_routes')(app);
+//require('./routes/app_routes')(app);
 
-app.listen(process.env.PORT || 3000);
+app.listen(process.env.PORT || 35728);

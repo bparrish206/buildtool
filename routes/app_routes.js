84271147975972518322df15b99e17547e939af0
bparrish206 <brentparrish@Rosias-MacBook-Pro.local>
2015-08-08 07:50:48 -0700

    so much work still to do

--- routes/app_routes.js
+++ routes/app_routes.js
@@ -2,6 +2,7 @@
 
 var request = require('superagent');
 var _ = require("underscore");
+var fs = require('fs');
 
 module.exports = function(app){
 app.post('/', function(req, res){
@@ -12,7 +13,6 @@ app.post('/', function(req, res){
       if(err) throw err;
       var title = purlData.body.title;
       var est = purlData.body.estimates;
-      console.log(est)
       var choice = est[0].choice;
       var value = est[0].value;
       var date = purlData.body.last_updated;
@@ -20,10 +20,15 @@ app.post('/', function(req, res){
       var test = _.pick(purlData.body, 'estimates_by_date');
       var test1 = _.values(test);
       var test2 = test1[0];
-      var test3 = _.map(test2, function(elm){return elm.date +" " + _.map(elm.estimates, function(est){return _.values(est)})});
-      //console.log(test3);
+      var test3 = _.map(test2, function(elm){return  elm.date +", " + _.map(elm.estimates, function(est){return _.values(est)})});
+      var test4 = test3.join(', ');
+      fs.writeFile('chart.csv', test4, function(err) {
+        if(err) throw err;
+        console.log("Chart has saved!");
+      });
+      //console.log(test2);
       var rdate = date.slice(0,10);
-    res.json({chart: title, date: rdate, choice: choice, value: value, test3: test3});
+    res.json({chart: title, date: rdate, choice: choice, value: value, test2: test2});
   });
 });
 };

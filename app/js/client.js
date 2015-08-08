fbb2905cdac390f4b9ce763f2f28ff89603a67c8
bparrish206 <brentparrish@Rosias-MacBook-Pro.local>
2015-08-07 23:31:04 -0700

    tweak

--- app/js/client.js
+++ app/js/client.js
@@ -4,7 +4,7 @@ var $ = require('jquery');
 
 $.post('/', function(data) {
   $('#app').append('<h4>' + data.chart + ' ' + data.date + ' ' + data.choice + ' ' + data.value + '%' + '</h4>');
-  $('#main').append('<p>' + data.test + '</p>');
+  $('#main').append('<p>' + data.test3 + '</p>');
   //var chrData = _.pick(data.block, 'data', 'estimates');
 
   //console.log(chrData);
--- routes/app_routes.js
+++ routes/app_routes.js
@@ -12,21 +12,18 @@ app.post('/', function(req, res){
       if(err) throw err;
       var title = purlData.body.title;
       var est = purlData.body.estimates;
-      var choice = est[1].choice;
-      var value = est[1].value;
+      console.log(est)
+      var choice = est[0].choice;
+      var value = est[0].value;
       var date = purlData.body.last_updated;
       var block = JSON.stringify(purlData.body);
       var test = _.pick(purlData.body, 'estimates_by_date');
       var test1 = _.values(test);
       var test2 = test1[0];
       var test3 = _.map(test2, function(elm){return elm.date +" " + _.map(elm.estimates, function(est){return _.values(est)})});
-      console.log(test3);
-      block = block.replace(/[{}]/g,'');
-      block = block.replace(/[""]/g,'');
-      block = block.replace(/[:]/g, ': ');
-      block = block.replace(/[,]/g, ', ');
+      //console.log(test3);
       var rdate = date.slice(0,10);
-    res.json({chart: title, date: rdate, choice: choice, value: value, test: test});
+    res.json({chart: title, date: rdate, choice: choice, value: value, test3: test3});
   });
 });
 };

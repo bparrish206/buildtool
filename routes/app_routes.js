09236915afe13a4217be8f003ae0db380de0d705
bparrish206 <brentparrish@Rosias-MacBook-Pro.local>
2015-08-07 22:15:38 -0700

    updating project, refining data to make charts

--- routes/app_routes.js
+++ routes/app_routes.js
@@ -1,6 +1,7 @@
 'use strict';
 
 var request = require('superagent');
+var _ = require("underscore");
 
 module.exports = function(app){
 app.post('/', function(req, res){
@@ -15,12 +16,17 @@ app.post('/', function(req, res){
       var value = est[1].value;
       var date = purlData.body.last_updated;
       var block = JSON.stringify(purlData.body);
+      var test = _.pick(purlData.body, 'estimates_by_date');
+      var test1 = _.values(test);
+      var test2 = test1[0];
+      var test3 = _.map(test2, function(elm){return elm.date +" " + _.map(elm.estimates, function(est){return _.values(est)})});
+      console.log(test3);
       block = block.replace(/[{}]/g,'');
       block = block.replace(/[""]/g,'');
       block = block.replace(/[:]/g, ': ');
       block = block.replace(/[,]/g, ', ');
       var rdate = date.slice(0,10);
-    res.json({chart: title, date: rdate, choice: choice, value: value, block: block});
+    res.json({chart: title, date: rdate, choice: choice, value: value, test: test});
   });
 });
 };
